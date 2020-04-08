3892f242c770f9840ab0008ab59b1893af2fcc4f
Nicole Liu <nicoleliu_@outlook.com>
2020-04-08 12:45:02 -0400

    Initial Commit

---
+++ Project1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Liu, Bojun on 2020-04-08.
+//  Copyright © 2020 Liu, Nicole. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
